68a3cbdd69670c2628b3e3b08fb671427c1ceb94
DGCodeWeb <dasg2506@icloud.com>
2024-08-01 18:21:33 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'mont': ['"Montserrat Variable"', 'sans-serif']
+			},
+
+			keyframes: {
+				wiggle:{
+					'0%': {transform: 'translate(-300px,0)'},
+					'100%': {transform: 'translate(400px,0)'},
+				}
+			}
+		},
+	},
+	plugins: [],
+}
